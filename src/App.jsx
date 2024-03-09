c7025330de035e01743ba3157f988e31d1dac150
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-09 11:24:29 +0530

    Api Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import { fetchDataFromApi } from './utils/api'
 
 function App() {
-  
-  return (
-    <div className='App'>
-      App
-    </div>
-  )
+
+    useEffect(() => {
+        apiTesting();
+    }, [])
+
+    const apiTesting = () => {
+        fetchDataFromApi('/movie/popular')
+        .then((res) => {
+            console.log(res);
+        })
+    }
+
+    return (
+        <div className='App'>
+            App
+        </div>
+    )
 }
 
 export default App

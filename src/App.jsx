210f47c90937d4d49497f3476389ecd6bff04d9a
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-09 12:09:20 +0530

    Folder and Routes set

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
 
 function App() {
 
@@ -22,10 +30,15 @@ function App() {
     }
 
     return (
-        <div className='App'>
-            App
-            {url?.total_pages}
-        </div>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/:mediaType/:id' element={<Details />} />
+                <Route path='/search/:query' element={<SearchResult />} />
+                <Route path='/explore/:mediaType' element={<Explore />} />
+                <Route path='*' element={<PageNotFound />} />
+            </Routes>
+        </Router>
     )
 }
 

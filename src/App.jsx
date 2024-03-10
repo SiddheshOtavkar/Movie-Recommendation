2a7ece5bd2bbebf82ed86c138fde240709bd8734
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-10 11:37:44 +0530

    Genres Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
-import { getApiConfiguration } from './store/homeSlice'
+import { getApiConfiguration, getGenres } from './store/homeSlice'
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
@@ -18,6 +18,7 @@ function App() {
 
     useEffect(() => {
         fetchApiConfig();
+        genresCall();
     }, [])
 
     const fetchApiConfig = () => {
@@ -33,6 +34,26 @@ function App() {
 
                 dispatch(getApiConfiguration(url))
             })
+    }
+
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((value) => {
+            promises.push(fetchDataFromApi(`/genre/${value}/list`));
+        });
+
+        const data = await Promise.all(promises); // dono ka response sath me dega
+        console.log(data);
+
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+
     }
 
     return (

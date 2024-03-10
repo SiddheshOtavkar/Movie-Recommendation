fb6b488b04bc00ca062cd620c4ec21b8266fafc6
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-10 10:31:14 +0530

    SwicthTabs,Trending,CircleRating

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import "./Home.scss"
 import HeroBanner from './heroBanner/HeroBanner'
+import Trending from './trending/Trending'
 
 const Home = () => {
-  return (
-    <div className='homePage'>
-      <HeroBanner />
-      <div style={{ height: 1000 }}>
-
-      </div>
-    </div>
-  )
+    return (
+        <div className='homePage'>
+            <HeroBanner />
+            <Trending />
+            <div style={{ height: 1000 }}>
+            </div>
+        </div>
+    )
 }
 
 export default Home

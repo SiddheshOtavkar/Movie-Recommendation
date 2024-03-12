3a0100da75bb825cd4e6eb365fd6e7ef7a7de669
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-12 21:09:25 +0530

    Img

--- src/components/lazyLoadImage/Img.jsx
+++ src/components/lazyLoadImage/Img.jsx
@@ -3,6 +3,13 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Img = ({ src, className }) => {
+
+    // Check if src is a string
+    // if (typeof src !== "string") {
+    //     console.error("Error: src prop must be a string.");
+    //     return null; // Return null or a placeholder if src is not a string
+    // }
+
     return (
         <LazyLoadImage
             className={className || ""}
